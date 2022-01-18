5919a872a6d2388315e1a266ed5a07620fe2ca15
mathi330 <mathilded@protonmail.com>
2022-01-18 11:19:18 -0500

    E1: Start exercise Where's sausage dog? new game

---
+++ exercises/wheres-sausage-dog-new-game-plus/js/script.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const NUM_ANIMAL_IMAGES = 10;
+const NUM_ANIMALS = 100;
+
+let animalImages = [];
+let animals = [];
+
+let sausageDogImage = undefined;
+let sausageDog = undefined;
+
+function preload() {
+  for (let i = 0; i < NUM_ANIMAL_IMAGES; i++) {
+    let animalImage = loadImage(`assets/images/animal${i}.png`);
+    animalImages.push(animalImage);
+  }
+
+  sausageDogImage = loadImage(`assets/images/sausage-dog.png`);
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  createAnimals();
+  createSausageDog();
+}
+
+function createAnimals() {
+  for (let i = 0; i < NUM_ANIMALS; i++) {
+    let x = random(0, width);
+    let y = random(0, height);
+    let animalImage = random(animalImages);
+    let animal = new Animal(x, y, animalImage);
+    animals.push(animal);
+  }
+}
+function createSausageDog() {
+  let x = random(0, width);
+  let y = random(0, height);
+  sausageDog = new SausageDog(x, y, sausageDogImage);
+}
+
+function draw() {
+  background(255, 255, 0);
+
+  for (let i = 0; i < animals.length; i++) {
+    animals[i].update();
+  }
+
+  sausageDog.update();
+}
+
+function mousePressed() {
+  sausageDog.mousePressed();
+}
