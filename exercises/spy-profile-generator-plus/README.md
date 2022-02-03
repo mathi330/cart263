1795819316edfcb9baa73479a26f583ba2821b34
mathi330 <mathilded@protonmail.com>
2022-02-03 17:19:40 -0500

    E3: Added states for if you got the password right or wrong, added key interactions
    - if password correct:
         - can change alias
         - can change weapon
    - if password incorrect:
         - can try a new password
         - can start a new profile

--- exercises/spy-profile-generator-plus/README.md
+++ exercises/spy-profile-generator-plus/README.md
@@ -4,3 +4,6 @@ This is the README file for the entire project. For more official projects you s
 
 image of eye:
 https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fimage.flaticon.com%2Ficons%2Fpng%2F512%2F37%2F37090.png&f=1&nofb=1
+
+image for top secret stamp:
+https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F2.bp.blogspot.com%2F-x7O1yrE7ki4%2FU2ZbybG9U6I%2FAAAAAAAABKs%2FmoF0-gOYqJ8%2Fs1600%2FTop-Secret.png&f=1&nofb=1
--- exercises/spy-profile-generator-plus/js/script.js
+++ exercises/spy-profile-generator-plus/js/script.js
@@ -5,6 +5,8 @@ Mathilde Davan
 
 "use strict";
 
+let state = `profile`; // profile, redacted
+
 let spyProfile = {
   name: `**REDACTED**`,
   alias: `**REDACTED**`,
@@ -22,6 +24,8 @@ let spyProfile = {
 let imgEye = undefined;
 let eye = undefined;
 
+let imgStampTopSecret = undefined;
+
 // The variable that contains the information of the profile
 let data = undefined;
 
@@ -37,6 +41,7 @@ preload the JSON files used in the program
 */
 function preload() {
   imgEye = loadImage(`assets/images/eye.png`);
+  imgStampTopSecret = loadImage(`assets/images/stamp-secret.png`);
 
   monsterData = loadJSON(
     `https://raw.githubusercontent.com/dariusk/corpora/master/data/mythology/monsters.json`
@@ -71,19 +76,29 @@ function setup() {
   if (data) {
     // asks for your password
     let password = prompt(`Agent! Enter your password!`);
+
     // sees if the password is correct
     if (password === data.password) {
       // if yes, the data is displayed
-      setSpyData();
-      passwordLength();
-      spyProfile.visiblePassword = `${spyProfile.invisiblePassword}`;
-    } // otherwise the data is not viewable
+      profileSession();
+    } else {
+      // otherwise go to a different state
+      state = `redacted`;
+    }
   }
   // if there is no info in the data
   else {
     // asks for your name to create a profile
     generateSpyProfile();
   }
+}
+
+/**
+ */
+function profileSession() {
+  setSpyData();
+  passwordLength();
+  spyProfile.visiblePassword = `${spyProfile.invisiblePassword}`;
 }
 
 /**
@@ -144,16 +159,102 @@ function generateSpyProfile() {
 Description of draw()
 */
 function draw() {
-  background(255); // white
+  if (state === `profile`) {
+    rightPassword();
+    instructionsRight();
+  } else if (state === `redacted`) {
+    wrongPassword();
+    instructionsWrong();
+  }
+}
 
+/**
+the visible profile when the password is right
+*/
+function rightPassword() {
+  background(200);
+
+  profileInformation();
+  eye.update();
+}
+
+/**
+what appears if you enter the wrong password
+ */
+function wrongPassword() {
+  background(200);
+
+  push();
+  translate(width / 4, height / 2);
+  rotate(-0.3);
+  image(imgStampTopSecret, 0, 0, width / 2, width / 2);
+  pop();
+}
+
+/**
+ */
+function instructionsWrong() {
+  // line separating the canvas in 2 equal parts
+  push();
+  stroke(50);
+  line(width / 2, 50, width / 2, height - 50);
+  pop();
+
+  // text settings
+  push();
+  fill(50);
+  textStyle(BOLD);
+  textFont(`Roboto Mono`); // font link in index
+  textSize(20);
+  textAlign(LEFT, TOP);
+  text(
+    `Press BACKSPACE to delete your account.
+
+Press TAB to go back to the password.
+`,
+    width / 2 + 50,
+    100
+  );
+  pop();
+}
+
+function instructionsRight() {
+  // line separating the canvas in 2 equal parts
+  push();
+  stroke(50);
+  line(width / 2, 50, width / 2, height - 50);
+  pop();
+
+  // text settings
+  push();
+  fill(50);
+  textStyle(BOLD);
+  textFont(`Roboto Mono`); // font link in index
+  textSize(20);
+  textAlign(LEFT, TOP);
+  text(
+    `Press BACKSPACE to delete your account.
+
+Press something...
+`,
+    width / 2 + 50,
+    100
+  );
+  pop();
+}
+
+/**
+profile information (left side of the canvas)
+*/
+function profileInformation() {
   // text displayed
   let profile = `** SPY PROFILE **
 
-Name: ${spyProfile.name}
-Alias: ${spyProfile.alias}
-Secret Weapon: ${spyProfile.secretWeapon}
-Country of Operation: ${spyProfile.countryOfOperation}`;
-  let thePassword = `Password: ${spyProfile.visiblePassword}`;
+NAME: ${spyProfile.name}
+ALIAS: ${spyProfile.alias}
+SECRET WEAPON: ${spyProfile.secretWeapon}
+COUNTRY OF OPERATION: ${spyProfile.countryOfOperation}`;
+  let thePassword = `PASSWORD: ${spyProfile.visiblePassword}`;
 
   // text settings
   push();
@@ -165,18 +266,38 @@ Country of Operation: ${spyProfile.countryOfOperation}`;
   text(profile, 100, 100); // profile
   text(thePassword, 100, height - 100); // password at the bottom of the page
   pop();
-
-  eye.update();
 }
 
 /**
 deleting the profile when pressing BACKSPACE on keyboard
+re-ask for password when pressing TAB
 */
 function keyPressed() {
+  // click BACKSPACE to delete profile
   if (keyCode === BACKSPACE) {
-    localStorage.removeItem(`spy-profile-data`);
-    setup();
-    passwordLength();
+    state = `profile`; // set state to profile
+    localStorage.removeItem(`spy-profile-data`); // empty all the data saved
+    setup(); // Go back to setup (to ask for your name)
+    passwordLength(); // empty the hidden password string and create a new one for the new password
+  }
+  // click TAB to re-ask your password if you got it wrong
+  else if (keyCode === TAB && state === `redacted`) {
+    state = `profile`; // set state to profile
+    setup(); // Go back to setup (to ask the password)
+  }
+
+  // click A to change your alias
+  else if (key === `a` && state === `profile`) {
+    // chooses a name of color from the paint color names JSON file
+    let paintColor = random(paintColorData.colors);
+    // chooses a monster from the monsters JSON file
+    let monster = random(monsterData.names);
+    spyProfile.alias = `The ${paintColor.color} ${monster}`;
+  }
+  // click W to change your weapon
+  else if (key === `w` && state === `profile`) {
+    // chooses a random "weapon" using the objects JSON file
+    spyProfile.secretWeapon = random(objectData.objects);
   }
 }
 
