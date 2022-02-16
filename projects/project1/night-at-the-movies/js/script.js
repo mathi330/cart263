eecfc9bc53eb5e52a7d6aaa9c6bdbdd1fa518ad8
mathi330 <mathilded@protonmail.com>
2022-02-16 16:36:39 -0500

    P1: Created a circle composed of 12 equal parts and made a random "slice" appear in red every time the mouse is pressed

--- projects/project1/night-at-the-movies/js/script.js
+++ projects/project1/night-at-the-movies/js/script.js
@@ -1,12 +1,51 @@
 /**
-Night at the Movies
-Mathilde Davan
+cut a circle into 12 equal parts and have a red slice be chosen randomly every time the mouse is pressed
 */
 
 "use strict";
 
-function preload() {}
+let twelfth = undefined;
+let numParts = 12;
+let chooseRedPosition = undefined;
 
-function setup() {}
+let size = 150;
 
-function draw() {}
+function setup() {
+  createCanvas(400, 400);
+  angleMode(RADIANS);
+
+  twelfth = HALF_PI / 3;
+
+  chooseRedPosition = Math.floor(random(12));
+}
+
+function draw() {
+  background(220);
+
+  // the 12 parts
+  for (let i = 0; i < 12; i++) {
+    parts(twelfth * i);
+  }
+
+  fill(255, 0, 0);
+  noStroke();
+  rotateTwelfth(twelfth * chooseRedPosition);
+}
+
+function parts(angle) {
+  stroke(220);
+  fill(255);
+  rotateTwelfth(angle);
+}
+
+function rotateTwelfth(angle) {
+  push();
+  translate(width / 2, height / 2);
+  rotate(angle);
+  arc(0, 0, size, size, PI + HALF_PI, -twelfth * 2, PIE);
+  pop();
+}
+
+function mousePressed() {
+  chooseRedPosition = Math.floor(random(12));
+}
