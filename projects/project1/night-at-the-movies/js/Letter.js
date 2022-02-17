cec1f167cc68d93a7c23613993844e695639c35d
mathi330 <mathilded@protonmail.com>
2022-02-17 17:16:04 -0500

    P1: "Translate" the word from letters into shapes

--- projects/project1/night-at-the-movies/js/Letter.js
+++ projects/project1/night-at-the-movies/js/Letter.js
@@ -1,44 +1,44 @@
 class Letter {
-  constructor(x, y, angle) {
-    this.x = x;
-    this.y = y;
+  constructor(letter) {
+    this.letter = letter;
+    this.x = undefined;
+    this.y = undefined;
     this.twelfth = HALF_PI / 3;
-    this.angle = angle;
     this.size = 150;
 
     // design of the letter
-    this.numDesign = random(10);
+    this.numDesign = 1;
+    this.radius = random(this.size / 2 - 10, this.size / 2 + 10);
+    this.rotation = PI + HALF_PI;
     this.designs = [];
-    this.designSize = random(this.size / 50, this.size / 10);
+    this.designSize = random(5, 10);
   }
 
-  designElements() {
-    for (let i = 0; i < this.numDesign; i++) {
-      push();
-      // ellipseMode(CORNER);
-      translate(PI + HALF_PI, -this.twelfth * 2);
-      noFill();
-      stroke(255, 0, 0);
-      rotate();
-      arc(
-        0,
-        0,
-        this.designSize,
-        this.designSize,
-        PI + HALF_PI,
-        -this.twelfth * 2
-      );
-      pop();
-    }
-  }
-
-  display(angle) {
+  designElements(angle) {
     push();
+    fill(255, 0, 0);
+    noStroke();
     translate(this.x, this.y);
+    rotate(this.twelfth * angle);
+    translate(this.radius, 0);
+    ellipse(0, 0, this.designSize);
+    pop();
+  }
+
+  display(x, y, angle) {
+    push();
+    // black line
+    translate(x, y);
     noFill();
     stroke(0);
     rotate(angle);
-    arc(0, 0, this.size, this.size, PI + HALF_PI, -this.twelfth * 2);
+    arc(0, 0, this.size, this.size, this.rotation, -this.twelfth * 2);
+
+    // points
+    fill(0);
+    rotate(this.rotation);
+    translate(this.radius, 0);
+    ellipse(0, 0, this.designSize);
     pop();
   }
 }
--- projects/project1/night-at-the-movies/js/script.js
+++ projects/project1/night-at-the-movies/js/script.js
@@ -2,15 +2,46 @@
 Night at the Movies
 Mathilde Davan
 
+  - use a web storage API to save the "language"
+  - find a way to learn how o get the letters of the alphabet and "letters" array and the letters
+  of the mystery word work together
 */
 
 "use strict";
 
-// let twelfth = undefined;
-let numParts = 12;
-let chooseRedPosition = undefined;
+// let letter;
+let alphabet = [
+  `a`,
+  `b`,
+  `c`,
+  `d`,
+  `e`,
+  `f`,
+  `g`,
+  `h`,
+  `i`,
+  `j`,
+  `k`,
+  `l`,
+  `m`,
+  `n`,
+  `o`,
+  `p`,
+  `q`,
+  `r`,
+  `s`,
+  `t`,
+  `u`,
+  `v`,
+  `w`,
+  `x`,
+  `y`,
+  `z`,
+];
+let numLetters = 26;
+let letters = [];
 
-let letter;
+let myWord = [];
 
 let words = undefined;
 let niceWords = undefined;
@@ -18,17 +49,7 @@ let mysteryWord = undefined;
 
 function preload() {
   words = loadJSON(`assets/json/words.json`);
-
-  // words = loadJSON(
-  //   `https://raw.githubusercontent.com/dariusk/corpora/master/data/words/common.json`
-  // );
-  //
-  // niceWords = loadJSON(
-  //   `https://raw.githubusercontent.com/dariusk/corpora/master/data/words/encouraging_words.json`
-  // );
 }
-
-// let size = 150;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -40,29 +61,71 @@ function setup() {
   }
   console.log(mysteryWord);
 
-  letter = new Letter(width / 2, height / 2);
-
-  chooseRedPosition = Math.floor(random(12));
+  for (let i = 0; i < numLetters; i++) {
+    let letter = new Letter(alphabet[i]);
+    letters.push(letter);
+  }
+  // wordLetters();
 }
 
 function draw() {
   background(240);
 
   // the 12 parts
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < mysteryWord.length; j++) {
+      // for (let k = 0; k < mysteryWord.length; k++) {
+      let wordLetter = mysteryWord.charAt(j);
+      // console.log(wordLetter);
+
+      if (letters[i].letter === wordLetter) {
+        myWord.push(letters[i]);
+      }
+
+      // letter.designElements(angle);
+      // }
+    }
+  }
+
   for (let i = 0; i < mysteryWord.length; i++) {
-    fill(255, 0, 0);
-    noStroke();
-    letter.display(letter.twelfth * i);
-    letter.designElements();
+    let angle = myWord[i].twelfth * i;
+    myWord[i].display(width / 4, height / 2, angle);
+  }
+  // for (let k = 0; k < myWord.length; k++) {
+  //   let angle = myWord[k].twelfth * k;
+  //   fill(255, 0, 0);
+  //   noStroke();
+  //   myWord[k].display(width / 2, height / 2, angle);
+  // }
+
+  decodingBook();
+}
+
+/**
+Information on what letters correspond to
+(Not working yet)
+*/
+function decodingBook() {
+  let a = 1;
+  let a2 = 4;
+  let b = 0;
+  for (let i = 0; i < letters.length / 2; i++) {
+    a++;
+    let letter = letters[i];
+
+    letter.display((width / 8) * a2 + i, (height / 8) * (1 + b));
+    if (a < 8) {
+      a2 = 4;
+      b++;
+    }
   }
 }
 
-function parts(angle) {
-  stroke(220);
-  fill(255);
-  rotateTwelfth(angle);
-}
-
-function mousePressed() {
-  chooseRedPosition = Math.floor(random(12));
+function wordLetters() {
+  for (let i = 0; i < letters.length; i++) {
+    for (let j = 0; j < mysteryWord.length; j++) {
+      let wordLetter = mysteryWord.charAt(j);
+      let letter = letters[i];
+    }
+  }
 }
