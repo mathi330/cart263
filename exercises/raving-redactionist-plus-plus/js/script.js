a04c200af42792ff7fb781b122f08a42024d0d20
mathi330 <mathilded@protonmail.com>
2022-03-09 13:23:25 -0500

    E6: Created buttons at random position on each side of the text

--- exercises/raving-redactionist-plus-plus/js/script.js
+++ exercises/raving-redactionist-plus-plus/js/script.js
@@ -2,29 +2,61 @@
 Raving Redactionist
 Mathilde Davan
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
 */
 
 "use strict";
 
-$(`.top-secret`).on(`click`, redact);
+// $(`.top-secret`).on(`click`, redact);
+//
+// setInterval(revelation, 500);
+//
+// function redact(event) {
+//   $(this).removeClass(`revealed`);
+//   $(this).addClass(`redacted`);
+// }
+//
+// function revelation() {
+//   $(`.redacted`).each(attemptReveal);
+// }
+//
+// function attemptReveal() {
+//   let r = Math.random();
+//   if (r < 0.05) {
+//     $(this).removeClass(`redacted`);
+//     $(this).addClass(`revealed`);
+//   }
+// }
 
-setInterval(revelation, 500);
+let $buttons = [];
+let $numButtons = 20;
 
-function redact(event) {
-  $(this).removeClass(`revealed`);
-  $(this).addClass(`redacted`);
-}
+let fifthOfBodyWidth = $(`body`).width() / 5;
 
-function revelation() {
-  $(`.redacted`).each(attemptReveal);
-}
+buttonsPositioning();
 
-function attemptReveal() {
-  let r = Math.random();
-  if (r < 0.05) {
-    $(this).removeClass(`redacted`);
-    $(this).addClass(`revealed`);
+function buttonsPositioning() {
+  for (let i = 0; i < $numButtons; i++) {
+    createButton(getRndInteger(10, fifthOfBodyWidth * 0.9));
+    createButton(
+      getRndInteger(fifthOfBodyWidth * 3.9, fifthOfBodyWidth * 5 - 60)
+    );
   }
+}
+
+function createButton(xPosition) {
+  let $button = $(`<button></button>`);
+  $button.css({
+    position: "absolute",
+    top: getRndInteger(20, $(`body`).height()),
+    left: xPosition,
+  });
+  $button.text(`Click Me!`);
+
+  $(`#button-section`).append($buttons);
+  $buttons.push($button);
+}
+
+// https://www.w3schools.com/JS/js_random.asp
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
