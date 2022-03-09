4195bb2d3c551f483a268687bf4b1aca5dc0f202
mathi330 <mathilded@protonmail.com>
2022-03-09 09:30:28 -0500

    E6: Started exercise raving redactionist ++

---
+++ exercises/raving-redactionist-plus-plus/js/script.js
@@ -0,0 +1,30 @@
+/**
+Raving Redactionist
+Mathilde Davan
+
+This is a template. You must fill in the title,
+author, and this description to match your project!
+*/
+
+"use strict";
+
+$(`.top-secret`).on(`click`, redact);
+
+setInterval(revelation, 500);
+
+function redact(event) {
+  $(this).removeClass(`revealed`);
+  $(this).addClass(`redacted`);
+}
+
+function revelation() {
+  $(`.redacted`).each(attemptReveal);
+}
+
+function attemptReveal() {
+  let r = Math.random();
+  if (r < 0.05) {
+    $(this).removeClass(`redacted`);
+    $(this).addClass(`revealed`);
+  }
+}
