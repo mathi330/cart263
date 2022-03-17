8ec385d3f0d6d2cc67cb2d1c3f3d5dcda6080a20
mathi330 <mathilded@protonmail.com>
2022-03-17 16:55:16 -0400

    E7: Changed the poem to letters randomly positioned

--- exercises/code-taker-plus-plus/js/script.js
+++ exercises/code-taker-plus-plus/js/script.js
@@ -2,25 +2,67 @@
 Code Taker
 Mathilde Davan
 
-This is a template. You must fill in the title,
-author, and this description to match your project!
+
 */
 
 "use strict";
 
-$(`.secret`).one(`mouseover`, function (event) {
-  $(this).addClass(`found`, 500);
-  $(this).draggable({
-    helper: `clone`,
+const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+let $letters = [];
+const numLetters = 200;
+
+createLetters();
+
+function createLetters() {
+  for (let i = 0; i < numLetters; i++) {
+    createLetter(getRndInteger(10, $(`body`).height() / 2 - 10));
+    createLetter(
+      getRndInteger($(`body`).height() / 2 + 53, $(`body`).height() - 20)
+    );
+  }
+}
+
+function createLetter(yPosition) {
+  let $letter = $(`<span></span>`);
+
+  $letter.css({
+    position: `absolute`,
+    left: getRndInteger(20, $(`body`).width() - 20),
+    top: yPosition,
   });
+
+  let randomNum = Math.floor(Math.random() * alphabet.length);
+  $letter.text(alphabet[randomNum]);
+
+  $letter.addClass(`secret`);
+
+  $letters.push($letter);
+  $(`#poem`).append($letters);
+}
+
+$(`.secret`).on(`mouseover`, function (event) {
+  $(this).addClass(`found`, 100);
+
+  $(this).draggable({
+    start: function (event, ui) {
+      $(this).addClass(`found`);
+    },
+
+    stop: function (event, ui) {
+      $(this).removeClass(`found`);
+    },
+  });
+});
+
+$(`.secret`).on(`mouseleave`, function (event) {
+  $(this).removeClass(`found`, 500);
 });
 
 $(`#answer`).droppable({
   drop: function (event, ui) {
     let letter = ui.draggable.text();
     $(this).append(letter);
-    ui.draggable.draggable(`disable`);
-    ui.draggable.removeClass(`found`, 500);
+    ui.draggable.remove();
     if ($(this).text() === `Theremin`) {
       $(`#solved-dialog`).dialog(`open`);
     }
@@ -28,10 +70,21 @@ $(`#answer`).droppable({
 });
 
 $(`#solved-dialog`).dialog({
-  autoOpen: false,
+  // autoOpen: false,
+  modal: true,
   buttons: {
-    "I know.": function () {
+    Okay: function () {
       $(this).dialog(`close`);
     },
   },
 });
+
+/**
+getRndInteger(min, max)
+random function taken from w3school that takes a random integral between a min and max determined when it is used
+
+https://www.w3schools.com/JS/js_random.asp
+*/
+function getRndInteger(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min; // return a random integral between the min and max
+}
