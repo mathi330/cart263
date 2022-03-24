468db22e36aa2f6fe8c891d87a1739551d86e0f9
mathi330 <mathilded@protonmail.com>
2022-03-24 17:17:10 -0400

    pro p2: Added jquery UI for dialog boxes, added a place for the user to enter their answer, started to look into how to display the code on the page

--- projects/project2/prototype/js/script.js
+++ projects/project2/prototype/js/script.js
@@ -13,10 +13,10 @@ const loremIpsumText = [
   `Vestibulum feugiat viverra leo, et fermentum risus gravida a.`,
   `Donec sollicitudin vitae enim nec pulvinar.`,
   `Duis sodales feugiat augue nec malesuada.`,
-  `Morbi blandit vestibulum arcu tincidunt faucibus.`,
+  `Morbi blandit vestibulum arcu tincidunt faukibus.`,
   `Aliquam pulvinar ornare sem, id blandit libero.`,
   `Donec congue neque odio, a consectetur neque sodales sit amet.`,
-  `Phasellus et enim faucibus, mollis nisi sed, tincidunt mi.`,
+  `Phasellus et enim faukibus, mollis nisi sed, tincidunt mi.`,
   `Aliquam pretium diam ac odio consectetur luctus.`,
   `Sed nec pellentesque ante.`,
   `Nam eu consequat ipsum, eu facilysis justo.`,
@@ -24,7 +24,7 @@ const loremIpsumText = [
   `Nam ac mauris sodales, accumsan nisl eget, commodo sapien.`,
   `Phasellus ultricies commodo blandit.`,
   `Donec vehicula ultrices velit, ac placerat nibh bibendum in.`,
-  `Nulla id wusto ut massa egestas vestibulum sit amet maximus nunc.`,
+  `Nulla id wusto ut mazza egestas vestibulum sit amet maximus nunc.`,
   `Sed et quam ante.`,
   `Ut et mi a quam ullamcorper tristique.`,
   `Nam interdum dui vitae risus volutpat maximus.`,
@@ -38,60 +38,6 @@ const loremIpsumText = [
   `Proin sit amet felis malesuada nibh aliquam venenatis ut eu nisl.`,
   `Cras velit ex, sagittis quis facilysis at, condimentum vitae elit.`,
   `Donec id diam pretium, pharetra mi sit amet, pharetra sem.`,
-  `Praesent sit amet lacus ac ipsum pretium accumsan.`,
-  `Duis odio leo, maximus ac dui ac, tincidunt varius ligula.`,
-  `Nulla malesuada nunc massa, sed pellentesque nisl faucibus eu.`,
-  `Suspendisse molestie lectus id magna lobortis, id lobortis nulla luctus.`,
-  `Aliquam nec risus massa.`,
-  `Donec convallis libero ipsum, nec bibendum odio malesuada vulputate.`,
-  `Morbi elementum tortor sem, wel vulputate eros dictum in.`,
-  `Cras in egestas leo, eget scelerisque lorem.`,
-  `Aliquam ex sem et ante sollicitudin aliquam.`,
-  `Duis lacinia nunc quis volutpat tristique.`,
-  `Nam wel vestibulum neque, sed elementum dolor.`,
-  `Nullam id est dignissim, suscipit dolor quis, dapibus eros.`,
-  `Nunc vel ante eget ante porttitor varius quis at dolor.`,
-  `Proin sit amet sollicitudin nisl.`,
-  `Maecenas a ultricies lorem, vel aliquam nulla.`,
-  `Suspendisse at malesuada nunc.`,
-  `Mauris ut nisi sit amet dui congue imperdiet eu eget mazza.`,
-  `Nullam ac nisl facilysis, tristique tortor ac, condimentum leo.`,
-  `Etiam non felis nulla.`,
-  `Suspendisse eu justo nibh.`,
-  `Vivamus ac vestibulum nibh.`,
-  `Donec quis leo cursus, aliquet tortor et, congue libero.`,
-  `Mauris nec vehicula magna.`,
-  `Quisque vitae bibendum risus.`,
-  `Nulla cursus augue id mi viverra viverra.`,
-  `Proin id ex ut ligula semper ultrices. Morbi wel quam nisi.`,
-  `Phasellus ut eros sit amet felis tincidunt congue.`,
-  `In hac habitasse platea dictumst.`,
-  `Integer wel placerat massa, id semper nisl.`,
-  `Maecenas et libero gravida, sollicitudin arcu eu, finibus sapien.`,
-  `Sed malesuada sapien sit amet metus aliquam, nec pretium dui maximus.`,
-  `Nunc non risus id nibh suscipit facilysis ut vel urna.`,
-  `Vestibulum pharetra purus nec lectus pulvinar aliquam.`,
-  `Interdum et malesuada fames ac ante ipsum primis in faucibus.`,
-  `Sed eu facilysis libero.`,
-  `Aliquam non efficitur odio.`,
-  `Aliquam ut ultricies elit.`,
-  `Aenean lacinia vehicula pulvinar.`,
-  `Proin malesuada tellus lacus, porttitor porttitorex risus mattis id.`,
-  `Duis pulvinar sapien id urna ultrices fermentum.`,
-  `Vivamus hendrerit tempor purus, wel suscipit justo sagittis sed.`,
-  `Integer semper neque imperdiet elit venenatis, id malesuada eros rutrum.`,
-  `Sed nec placerat ante, et euismod leo.`,
-  `Donec aliquet est ipsum, at volutpat est tincidunt vitae.`,
-  `Morbi leo velit, faucibus sit amet viverra eu, bibendum ac ex.`,
-  `Quisque maximus ultrices nibh sed suscipit.`,
-  `In ornare eu feliz et luctus.`,
-  `Proin efficitur tincidunt ipsum, a faucibus tortor lacinia a.`,
-  `Quisque cursus odio ac enim scelerisque condimentum.`,
-  `Vestibulum non elementum est.`,
-  `Aenean ac risus bibendum, lacinia nulla quis, mollis sem.`,
-  `Duis eu orci id nibh porta feugiat sed et justo.`,
-  `Nulla suscipit tincidunt lacus, nec dictum massa dictum szelerisque.`,
-  `Etiam feugiat facilysis tincidunt.`,
 ];
 
 let jsonFile = undefined;
@@ -103,6 +49,67 @@ $.getJSON(`/assets/data/common.json`).done(function data(data) {
   // function to call when the file is loaded successfully
   jsonFile = data;
   processResult(jsonFile);
+});
+
+// make the text into a dialog box
+$(function () {
+  $("#text").dialog({
+    autoOpen: false, // doesn't open at the start
+    height: 400,
+    width: 500,
+  });
+});
+
+// make the answer box into a dialog box
+$(function () {
+  $("#entry-box").dialog({
+    autoOpen: false, // doesn't open at the start
+    height: 300,
+    width: 300,
+  });
+});
+
+// make the 2 dialog box open and close when the designated button is clicked
+openCloseDialog(`#open-text-dialog`, `#text`);
+openCloseDialog(`#open-answer-dialog`, `#entry-box`);
+
+/**
+openCloseDialog(button, dialogBox)
+
+function to open and close a dialog box when its designated button is clicked
+*/
+function openCloseDialog(button, dialogBox) {
+  let textHidden = true; // see if the dialog is displayed
+
+  // when the button is clicked
+  $(button).on(`click`, function (event) {
+    // if the dialog box is closed
+    if (textHidden) {
+      $(dialogBox).dialog("open"); // open it
+      textHidden = false;
+    }
+    // if the dialog box is open
+    else {
+      $(dialogBox).dialog("close"); // close it
+      textHidden = true;
+    }
+  });
+}
+
+// look at the answer the user inputed into the answer box when the submit button is pressed
+$(`#submit-answer`).on(`click`, function (event) {
+  // the user's answer
+  let userAnswer = $(`#user-answer`).val();
+  // if the answer is correct
+  if (userAnswer === chosenWord) {
+    // alert with the answer (for now to test)
+    alert(userAnswer);
+  }
+  // if the answer is wrong
+  else {
+    // alert with "Wrong!" (for now)
+    alert(`Wrong!`);
+  }
 });
 
 /**
@@ -135,7 +142,13 @@ function processResult(data) {
     // add the letter and it's position in the lorem ipsum to the array for the final chosen position
     listOfSelectedPos.push([chosenWord[i], listOfPositions[selectedPosition]]);
     // show the position of the letter
-    console.log(`letter position` + `:` + listOfPositions[selectedPosition]);
+    console.log(
+      listOfSelectedPos[i][0] + `:` + listOfPositions[selectedPosition]
+    );
+
+    // display the code on the page
+    let codeString = listOfPositions[selectedPosition];
+    $(`#secret-code`).append(`${codeString}`);
   }
   // add the lorem ipsum text
   addLoremIpsum(listOfSelectedPos);
@@ -175,7 +188,7 @@ function findCharacterPos(char) {
 }
 
 /**
-addLoremIpsum(listOfLetterPos)\
+addLoremIpsum(listOfLetterPos)
 
 display the lorem ipsum text and add a span around the letter that we'll use in the book cipher
 */
@@ -210,7 +223,7 @@ function addLoremIpsum(listOfLetterPos) {
   });
 
   // section in the html to which we want to add the text
-  const sectionText = $(`#lorem-ipsum`);
+  const sectionText = $(`#text`);
 
   // for each paragraph of lorem ipsum
   for (let i = 0; i < loremIpsumText.length; i++) {
